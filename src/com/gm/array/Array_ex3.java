e3915f78fefbc5652c380f5e2d59d5e3c69abfbd
Guma-99 <skc06040@naver.com>
2022-07-12 17:48:30 +0900

    0712_Array_ex3(미완)

--- src/com/gm/array/Array_ex3.java
+++ src/com/gm/array/Array_ex3.java
@@ -19,66 +19,101 @@ public class Array_ex3 {
 
 		while (check) {
 			System.out.println("=====학생 성적 프로그램=====");
-			System.out.println("1. 학생 정보 입력");
-			System.out.println("2. 학생 정보 조회");
-			System.out.println("3. 학생 정보 검색");
-			System.out.println("4. 학생 정보 삭제");
-			System.out.println("5. 학생 정보 추가");
-			System.out.println("6. 프로그램 종료");
+			System.out.println("1. 2. 3. 4. 5. 6.");
 			int nums = sc.nextInt();
 
-			//1. 학생 정보 입력
-			//	 - 학생 수 입력
-			//	 - 이름, 번호, 국어, 수학, 영어 입력 후 총점, 평균 계산
+			// 1. 학생 정보 입력
+			// - 학생 수 입력
+			// - 이름, 번호, 국어, 수학, 영어 입력 후 총점, 평균 계산
 			if (nums == 1) {
 				System.out.println("학생 수 입력");
 				int study_num = sc.nextInt();
 
 				name = new String[study_num]; // 이름
-				number = new int[study_num]; // 번호
-				kor = new int[study_num]; // 국어 점수
-				math = new int[study_num]; // 수학 점수
-				eng = new int[study_num]; // 영어 점수
-				total = new int[study_num]; // 총점
-				avgs = new double[study_num]; // 평균
+				number = new int[name.length]; // 번호
+				kor = new int[name.length]; // 국어 점수
+				math = new int[name.length]; // 수학 점수
+				eng = new int[name.length]; // 영어 점수
+				total = new int[name.length]; // 총점
+				avgs = new double[name.length]; // 평균
 
 				for (int i = 0; i < name.length; i++) {
 					System.out.println(i + 1 + "번 학생 이름 입력");
 					name[i] = sc.next();
+					System.out.println(name[i] + "의 번호 입력");
+					number[i] = sc.nextInt();
 					System.out.println(name[i] + "의 국어 점수 입력");
 					kor[i] = sc.nextInt();
 					System.out.println(name[i] + "의 수학 점수 입력");
 					math[i] = sc.nextInt();
 					System.out.println(name[i] + "의 영어 점수 입력");
 					eng[i] = sc.nextInt();
+					System.out.println("=====입력 완료=====");
+					System.out.println("");
 
-				}
-
-				for (int i = 0; i < study_num; i++) {
-					total[i] = 0;
 					total[i] = total[i] + (kor[i] + math[i] + eng[i]);
 					avgs[i] = total[i] / 3.0;
+				}
+			} // 1번 끝
 
-					System.out.println(name[i] + "의 총점: " + total[i]);
-					System.out.println(name[i] + "의 평균: " + avgs[i]);
-				}
-			} //1번 끝 
-			
-			//2. 학생 정보 조회
-//			 - 모든 학생의 이름, 번호, 총점, 평균 출력
+			// 2. 학생 정보 조회
+			// - 모든 학생의 이름, 번호, 총점, 평균 출력
 			else if (nums == 2) {
-				for (int i = 0; i < name.length; i++) {
-					System.out.println(name[i] + "총점: " + total[i]);
+				if (name != null) {
+					for (int i = 0; i < name.length; i++) {
+						System.out.println("이름\t 번호\t 국어\t 수학\t 영어\t 총점\t 평균");
+						System.out.println(name[i] + "\t " + number[i] + "\t " + kor[i] + "\t " + math[i] + "\t "
+								+ eng[i] + "\t " + total[i] + "\t " + avgs[i]);
+					}
+					System.out.println("=====조회 완료=====");
+					System.out.println("");
+				} else {
+					System.out.println("=====학생 정보가 없습니다.=====");
+					System.out.println("");
 				}
-			} //2번 끝
-			
-			//6. 프로그램 종료
+			} // 2번 끝
+
+			// 3. 학생 정보 검색
+			// - 검색할 학생의 번호 입력
+			// - 입력한 번호와 일치하는 학생의 모든 정보 출력
+			// - 일치하는 번호가 없으면 '없는 학생이다' 출력
+			else if (nums == 3) {
+				System.out.println("검색할 학생 번호 입력");
+				int study_num = sc.nextInt();
+				boolean flag = false;
+
+				for (int i = 0; i < name.length; i++) {
+					if (study_num == number[i]) {
+						System.out.println("이름\t 번호\t 국어\t 수학\t 영어\t 총점\t 평균");
+						System.out.println(name[i] + "\t " + number[i] + "\t " + kor[i] + "\t " + math[i] + "\t "
+								+ eng[i] + "\t " + total[i] + "\t " + avgs[i]);
+						System.out.println("=====검색 완료=====");
+						System.out.println("");
+						flag = !flag;
+						break;
+					}
+				}
+					if (flag) {
+						System.out.println("=====일치하는 학생 정보가 없습니다.=====");
+						System.out.println("");
+					}
+			} // 3번 끝
+
+
+			// 6. 프로그램 종료
 			else if (nums == 6) {
 				break;
 			}
 		} // while 끝
 	}
 }
+
+// System.out.println("1. 학생 정보 입력");
+// System.out.println("2. 학생 정보 조회");
+// System.out.println("3. 학생 정보 검색");
+// System.out.println("4. 학생 정보 삭제");
+// System.out.println("5. 학생 정보 추가");
+// System.out.println("6. 프로그램 종료");
 
 //학생 성적 프로그램
 //1. 학생 정보 입력
